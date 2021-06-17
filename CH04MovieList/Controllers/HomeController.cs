0580937e71d7cb0c6672b8da5fa718771592127d
Colby Boell <cmboell1221@gmail.com>
2021-06-17 18:16:10 -0500

    Add project files.

---
+++ CH04MovieList/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CH04MovieList.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CH04MovieList.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private MovieContext context { get; set; }
+
+        public HomeController(MovieContext ctx)
+        {
+            context = ctx;
+        }
+        public IActionResult Index()
+        {
+            var movies = context.Movies.Include(m => m.Genre).OrderBy(m => m.Name).ToList();
+            return View(movies);
+        }
+
+    }
+}
